12fd2315d729e5ff9be1ff506b64c3e3a8bc46dd
Jas <doggrell@gmail.com>
2022-09-10 14:59:21 +0100

    Initial conversion always returns I

---
+++ RomanNumerals.Tests/NumbersToRomanNumeralsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace RomanNumerals.Tests
+{
+    public class NumbersToRomanNumeralsTest
+    {
+        private NumbersToRomanNumerals _numbersToRomanNumerals;
+        [SetUp]
+        public void Setup()
+        {
+            _numbersToRomanNumerals = new NumbersToRomanNumerals();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _numbersToRomanNumerals.UnderTenCalculator().Should().Be("I");
+        }
+    }
+
+}
